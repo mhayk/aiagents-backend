5da48a11ffc043425ea36671e704cb2e57ee7b37
Mhayk Whandson <mhayk@kaumediagroup.com>
2025-07-14 14:26:03 +0100

    feat: configure drizzle

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ai Agent Server
+
+## Drizzle ORM
+```
+$ npx drizzle-kit generate
+$ npx drizzle-kit migrate
+$ npx drizzle-kit studio
+```
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,14 @@
 import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { env } from "../env.ts";
+import { schema } from "./schema/index.ts";
 
 export const sql = postgres(env.DATABASE_URL)
 
 const result = await sql`SELECT 'Hello' AS message`;
+export const db = drizzle(sql, {
+    schema,
+    casing: 'snake_case'
+});
 
 console.log(result)
